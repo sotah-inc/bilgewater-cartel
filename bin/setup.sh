557a19bc71632f4fd23919a6c4de0617b648c3e9
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-12-25 10:02:38 -0700

    Refactor to use shared pvc.

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,6 +9,7 @@ MODE=$1
   && ./bin/apply-template.sh -t grafana -m "$MODE" -a \
   && ./bin/apply-template.sh -t nats -m "base" -a \
   && ./bin/apply-template.sh -t postgresql -m "$MODE" -a \
-  && ./bin/apply-template.sh -t sotah-server-api -m "$MODE" -a \
+  && ./bin/apply-template.sh -t sotah-server-pvc -m "$MODE" -a \
+  && ./bin/apply-template.sh -t sotah-server-api -m "base" -a \
   && ./bin/apply-template.sh -t sotah-api -m "$MODE" -a \
   && ./bin/apply-template.sh -t sotah-ui -m "base" -a
